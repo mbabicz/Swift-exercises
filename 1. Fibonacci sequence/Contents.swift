4c3332db4445032e7393ea8e34eb99d59e11f792
Michał Babicz <49866616+mbabicz@users.noreply.github.com>
2023-01-24 21:53:43 +0100

    Fibonacci sequence

---
+++ 1. Fibonacci sequence/Contents.swift
@@ -0,0 +1,14 @@
+//Write a method that returns the nth element of the Fibonacci Sequence
+
+
+//1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+
+//f(n) = f(n-1) - f(n+1)
+
+func fibonacci1(n: Int) -> Int {
+    guard n > 2 else { return 1}
+    return fibonacci1(n: n-1) + fibonacci1(n: n-2)
+}
+
+fibonacci1(n: 10)
+
