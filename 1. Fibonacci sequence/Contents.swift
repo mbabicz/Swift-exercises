83d8c6b04ee62b6b77115925286b95a083d96772
Michał Babicz <49866616+mbabicz@users.noreply.github.com>
2023-01-25 09:46:58 +0100

    Update Contents.swift

--- 1. Fibonacci sequence/Contents.swift
+++ 1. Fibonacci sequence/Contents.swift
@@ -8,7 +8,18 @@
 func fibonacci1(n: Int) -> Int {
     guard n > 2 else { return 1}
     return fibonacci1(n: n-1) + fibonacci1(n: n-2)
+    
 }
 
 fibonacci1(n: 10)
 
+func fibonacciSeries(n: Int) -> [Int]{
+    var array = [Int]()
+    for index in 0...n{
+        let output = fibonacci1(n: index)
+        array.append(output)
+    }
+    return array
+}
+
+fibonacciSeries(n: 10)
