b1ea729fa656a1422e0ad2e9815f1ce6e90b8c18
Michał Babicz <kiwuzx@gmail.com>
2023-01-30 16:00:54 +0100

    Add files via upload

---
+++ 7. Palindrome number/Contents.swift
@@ -0,0 +1,17 @@
+//Check if number is palindrome
+
+func isPalindrome(number: Int) -> Bool {
+    var stringNum = String(number)
+    var resersedStringNum = String(stringNum.reversed())
+    
+    return stringNum == resersedStringNum
+    
+}
+
+isPalindrome(number: 10) //false
+isPalindrome(number: -11) //false
+isPalindrome(number: 12) //false
+isPalindrome(number: 11) //true
+isPalindrome(number: 121) //true
+isPalindrome(number: 123) //true
+
