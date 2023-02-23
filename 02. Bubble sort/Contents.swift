1d9b6876499c49bf513fb712024a0d701268627e
Michał Babicz <kiwuzx@gmail.com>
2023-02-23 19:56:34 +0100

    Bubble sort updated

--- 02. Bubble sort/Contents.swift
+++ 02. Bubble sort/Contents.swift
@@ -1,10 +1,6 @@
-//Write a program to sort list of elements using the bubble sort algoritm
-//numbers [3,6,5,1,7,8,4]
-//expected [1, 3, 4, 5, 6, 7, 8]
+//Write a program to sort list of elements using the bubble sort algorithm
 
-var exampleNumbers = [3,6,5,1,7,8,4]
-
-func bubbleSort(numbers: [Int]) -> [Int]{
+func bubbleSort(_ numbers: [Int]) -> [Int]{
     var sortedNumbers = numbers
     for _ in 0...exampleNumbers.count{
         for j in 1...sortedNumbers.count - 1 {
@@ -19,6 +15,5 @@ func bubbleSort(numbers: [Int]) -> [Int]{
     return sortedNumbers
 }
 
-print(exampleNumbers)
-
-bubbleSort(numbers: exampleNumbers)
+var exampleNumbers = [3,6,5,1,7,8,4]
+bubbleSort(exampleNumbers) // [1, 3, 4, 5, 6, 7, 8]
