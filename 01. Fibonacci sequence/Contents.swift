48542b876ca660154c2a7e0cb7fe3b9b64bcc476
Michał Babicz <kiwuzx@gmail.com>
2023-02-23 19:51:49 +0100

    fibonacci sequence updated

--- 01. Fibonacci sequence/Contents.swift
+++ 01. Fibonacci sequence/Contents.swift
@@ -1,25 +1,27 @@
-//Write a method that returns the nth element of the Fibonacci Sequence
+/*
+ Write a function that returns the nth element of the Fibonacci Sequence (1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89)
+f(n) = f(n-1) - f(n-2)
+ */
 
-
-//1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
-
-//f(n) = f(n-1) - f(n+1)
-
-func fibonacci1(n: Int) -> Int {
+func fibonacci(_ n: Int) -> Int {
     guard n > 2 else { return 1}
-    return fibonacci1(n: n-1) + fibonacci1(n: n-2)
-    
+    return fibonacci(n-1) + fibonacci(n-2)
 }
 
-fibonacci1(n: 10)
-
-func fibonacciSeries(n: Int) -> [Int]{
-    var array = [Int]()
-    for index in 0...n{
-        let output = fibonacci1(n: index)
-        array.append(output)
+func fibonacciSeries(_ n: Int) -> [Int]{
+    guard n > 0 else { return [] }
+    var array = [1]
+    
+    if n > 1 {
+        array.append(1)
+        for index in 2..<n {
+            let output = array[index-1] + array[index-2]
+            array.append(output)
+        }
     }
+    
     return array
 }
 
-fibonacciSeries(n: 10)
+fibonacci(10) // 55
+fibonacciSeries(10) // [1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
