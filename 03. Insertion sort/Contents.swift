d6f4e6eab46b718cd6dc3b82e1b3f3df44ac955d
Michał Babicz <kiwuzx@gmail.com>
2023-02-23 20:04:42 +0100

    Insertion sort fixed

--- 03. Insertion sort/Contents.swift
+++ 03. Insertion sort/Contents.swift
@@ -1,15 +1,9 @@
-//Write analgoritm for insertion sort
-
-//numbers [5, 2, 1, 6, 3, 7, 8, 9]
-//expected output: [1, 2, 3, 5, 6, 7, 8, 9]
-
-
-var numbers: [Int] = [5,2,1,6,3,7,8,9]
+//Write algorithm for insertion sort
 
 func insertionSort(numbers: [Int]) -> [Int]{
     var sortedNumbers = numbers
     
-    for i in 1...sortedNumbers.count{
+    for i in 1..<sortedNumbers.count{
         var index = i
         while index > 0{
             if sortedNumbers[index] < sortedNumbers[index-1]{
@@ -20,12 +14,9 @@ func insertionSort(numbers: [Int]) -> [Int]{
                 break
             }
         }
-        print(sortedNumbers)
     }
     return sortedNumbers
 }
 
-print(numbers)
-
-insertionSort(numbers: numbers)
-
+var numbers: [Int] = [5,2,1,6,3,7,8,9]
+insertionSort(numbers: numbers) // [1, 2, 3, 5, 6, 7, 8, 9]
