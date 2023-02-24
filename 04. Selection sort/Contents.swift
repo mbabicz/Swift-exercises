6ff7caefd7cd43c5907c5072131366227403accd
Michał Babicz <kiwuzx@gmail.com>
2023-02-24 22:13:07 +0100

    selection sort refactored

--- 04. Selection sort/Contents.swift
+++ 04. Selection sort/Contents.swift
@@ -1,18 +1,12 @@
-//Write an algoritm for selection sort
+//Write an algorithm for selection sort
 
-//numbers [3, 1, 5, 1, 7, 8, 3, 6, 2, 0]
-//expected output: [0, 1, 1, 2, 3, 3, 5, 6, 7, 8]
-
-
-var numbers: [Int] = [3,1,5,1,7,8,3,6,2,0]
-
-func selectionSort(numbers: [Int]) -> [Int]{
+func selectionSort(_ numbers: [Int]) -> [Int]{
     var sortedNumbers = numbers
     
-    for i in 0...numbers.count-1{
-        var lowestValueIndex: Int = i
+    for i in 0..<numbers.count{
+        var lowestValueIndex = i
         
-        for j in (i+1)..<numbers.count{
+        for j in (i + 1)..<numbers.count{
             if sortedNumbers[j] < sortedNumbers[lowestValueIndex]{
                 lowestValueIndex = j
             }
@@ -21,13 +15,9 @@ func selectionSort(numbers: [Int]) -> [Int]{
         if i != lowestValueIndex {
             sortedNumbers.swapAt(i, lowestValueIndex)
         }
-        
     }
-        
     return sortedNumbers
 }
 
-print(numbers)
-
-selectionSort(numbers: numbers)
-
+var numbers: [Int] = [3,1,5,1,7,8,3,6,2,0]
+selectionSort(numbers) // [0, 1, 1, 2, 3, 3, 5, 6, 7, 8]
