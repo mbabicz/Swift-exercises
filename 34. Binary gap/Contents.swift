902b7a79540c0251ce5645d98c842025ea5c1f7b
Michał Babicz <kiwuzx@gmail.com>
2023-03-25 22:38:02 +0100

    Update Contents.swift

--- 34. Binary gap/Contents.swift
+++ 34. Binary gap/Contents.swift
@@ -1,39 +1,30 @@
 //Find longest sequence of zeros in binary representation of an integeer
 
-public func solution(_ N : Int) -> Int {
-    // Implement your solution here
+public func solution(_ N: Int) -> Int {
     var decimal = N
-    var binary = ""
-    
+    var binary = String(decimal, radix: 2)
+
     var gapCount = 0
     var maxGap = 0
     var startedCounting = false
     
-    binary = String(decimal, radix: 2)
-    print("binary: \(binary)")
-    
     for i in binary {
-        if i == "1"{
-            if !startedCounting {
-                startedCounting = true
+        if i == "1" {
+            if startedCounting {
+                maxGap = max(maxGap, gapCount)
+                gapCount = 0
             } else {
-                if gapCount >= maxGap{
-                    maxGap = gapCount
-                }
+                startedCounting = true
             }
-            gapCount = 0
         } else {
-            if startedCounting{
+            if startedCounting {
                 gapCount += 1
             }
         }
     }
-    print("maxGap: \(maxGap)")
+
     return maxGap
 }
 
 solution(200)
-// Output:
-// binary: 11001000
-// maxGap: 2
-
+// Output: 2
