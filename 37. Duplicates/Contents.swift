5a1167edd8719410740088f9358dea21ceb243cd
Michał Babicz <kiwuzx@gmail.com>
2023-03-25 22:34:18 +0100

    Update Contents.swift

--- 37. Duplicates/Contents.swift
+++ 37. Duplicates/Contents.swift
@@ -1,24 +1,17 @@
 //Write a function that find duplicate number
 
 func findDuplicateNumber(_ array: [Int]) -> [Int] {
-    var duplicates = [Int]()
-    
     var numbers = [Int: Int]()
-    
-    for num in 0..<array.count{
-        if let count = numbers[array[num]] {
-            numbers[array[num]] = count + 1
-        }
-        else {
-            numbers[array[num]] = 1
-        }
+    var duplicates = [Int]()
+
+    for num in array {
+        numbers[num, default: 0] += 1
     }
-    
-    for (num, count) in numbers {
-        if count > 1 {
-            duplicates.append(num)
-        }
+
+    for (num, count) in numbers where count > 1 {
+        duplicates.append(num)
     }
+
     return duplicates
 }
 
